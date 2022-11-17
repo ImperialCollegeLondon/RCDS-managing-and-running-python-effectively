160473b1b823f9d9c6041768ef8ac0e56b917d49
coolernato <cmcooling@gmail.com>
2022-11-17 14:49:13 +0000

    Minor edits

--- Examples/Flags/quadratic.py
+++ Examples/Flags/quadratic.py
@@ -1,7 +1,7 @@
 # A function which returns the result of a quadratic expression
 def quadratic(a, b, c, x):
     # Raise an AssertionError if x is negative. Allows us to check x is zero or greater. Disabled if the -O flag is used
-    assert(x >= 0)
+    assert x >= 0
 
     # Calculate the expression and return it
     return a * x ** 2 + b * x + c
