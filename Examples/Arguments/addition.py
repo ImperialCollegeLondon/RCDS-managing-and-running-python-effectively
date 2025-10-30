932f8163c609d1306c95684c6d449d1c64b4bed3
Chris Cooling <cmcooling@gmail.com>
2025-10-30 15:15:56 +0000

    Renamed "sum" to "total" to avoid name clash

--- Examples/Arguments/addition.py
+++ Examples/Arguments/addition.py
@@ -3,12 +3,12 @@ import sys
 # Print out the name of the file being run and the command line arguments
 print(sys.argv)
 
-# Set sum to zero and accumulate the sum of the command line arguments in it
-sum = 0
+# Set total to zero and accumulate the sum of the command line arguments in it
+total = 0
 
-# Loop over the command line arguments and add them to sum
+# Loop over the command line arguments and add them to total
 for value in sys.argv[1:]:
-    sum = sum + int(value)
+    total = total + int(value)
 
 # Print the final value - the sum of all command line arguments
-print(sum)
+print(total)
